822197cc0cba8d0ea90821feb1e6c18ed8d3d43b
elaineseattle <84435178+elaineseattle@users.noreply.github.com>
2023-10-12 14:00:34 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
 import React, { useState, useCallback } from 'react';
 import './App.css';
 import 'h8k-components';
-import { recent, upvote } from './components/Articles'
 import Articles from './components/Articles';
+import { recent, upvote } from './components/Articles';
 
 const title = "Sorting Articles";
 
 function App({ articles }) {
-    const [filter, setFilter] = useState('recent');
+  // State for the currently selected filter
+  const [filter, setFilter] = useState('upvote');
 
-    const switchToFilter = useCallback((newFilter) => {
-        if (filter !== newFilter) {
-            setFilter(newFilter)
-        }
-    }, [filter])
+  // Function to switch the filter
+  const switchToFilter = useCallback((newFilter) => {
+    if (filter !== newFilter) {
+      setFilter(newFilter);
+    }
+  }, [filter]);
 
-    return (
-        <div className="App">
-            <h8k-navbar header={title}></h8k-navbar>
-            <div className="layout-row align-items-center justify-content-center my-20 navigation">
-                <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" onClick={() => switchToFilter(upvote)} className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" onClick={() => switchToFilter(recent)} className="small">Most Recent</button>
-            </div>
-            <Articles articles={articles} filter={filter} />
-        </div>
-    );
+  return (
+    <div className="App">
+      {/* Header */}
+      <h8k-navbar header={title}></h8k-navbar>
 
+      {/* Filter buttons */}
+      <div className="layout-row align-items-center justify-content-center my-20 navigation">
+        <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
+        <button data-testid="most-upvoted-link" onClick={() => switchToFilter(upvote)} className="small">Most Upvoted</button>
+        <button data-testid="most-recent-link" onClick={() => switchToFilter(recent)} className="small">Most Recent</button>
+      </div>
+
+      {/* Render the Articles component */}
+      <Articles articles={articles} filter={filter} />
+    </div>
+  );
 }
 
 export default App;
